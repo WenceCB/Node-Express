a2301c9b96c4cbc8b37f67974545b5a188780af8
Wence Criado Baez <wence74@gmail.com>
2017-02-04 12:55:43 +0100

    Aplicación que arranca servidor y escucha peticiones tipo GET

--- pruebaExpress/index.js
+++ pruebaExpress/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by Wence on 4/2/17.
  */
+
+var express = require('express');
+
+// Crear una app de express
+
+var app = express();
+
+// Escuchar peticiones en la raiz de nuestro sitito "/"
+
+app.get('/', function (req, res){
+    console.log('Petición');
+    // Vamos a responder las peticiones
+    res.send('Hola');
+
+});
+
+var server = app.listen(3000, function(){
+    var port = server.address().port;
+   console.log('Servidor Express Arrancado en el puerto '+ port);
+});
